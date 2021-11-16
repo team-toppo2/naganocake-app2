7635e97b9573eb446ba2a96063000db3398c4a14
ryooookada <btbl70120@gmail.com>
2021-11-16 08:27:59 +0000

    add

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,28 @@
 Rails.application.routes.draw do
   devise_for :users
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  # 会員側のルーティング設定
+  root to: 'public/homes#top'
+  get '/about' => 'public/homes#about'
+  resources :products, only: [:index, :show]
+  resources :customers, only: [:edit, :show, :update]
+  get 'customers/unsubscribe' => 'public/customers#unsubscribe'  
+  patch 'customers/withdraw' => 'public/customers#withdraw'
+  resources :carts_items, only: [:index, :create, :destroy, :update]
+  delete 'carts_items/all_destroy' => 'public/carts_items#all_destroy'
+  resources :orders, only: [:index, :create, :show, :new]
+  post 'orders/comfirm' => 'orders#comfirm'
+  get 'orders/complete' => 'orders#complete'
+  resources :addresses, only: [:index, :create, :edit, :update, :destroy]
+  
+  # 管理者側のルーティング設定
+  namespace :admin do
+    get '/admin' => 'admin/homes#top'
+    resources :products, only: [:index, :show, :create, :edit, :update, :new]
+    resources :genres, only: [:index, :create, :edit, :update]
+    resources :customers, only: [:index, :show, :edit, :update]
+    resources :orders, only: [:show, :update]
+    resources :order_details, only: [:update]
+  end
+
 end
