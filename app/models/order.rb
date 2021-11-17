9599606cf5c1048c4155e865f8e4e8b98cf135a9
ryooookada <btbl70120@gmail.com>
2021-11-17 04:44:48 +0000

    add

--- app/models/order.rb
+++ app/models/order.rb
@@ -2,6 +2,8 @@ class Order < ApplicationRecord
   
   
   belongs_to :customer
-  has_many :order_products, dependent: :destroy 
+  has_many :order_products, dependent: :destroy
+  
+  has_many :products, through: :order_products
   
 end
