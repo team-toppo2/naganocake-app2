b9af1fbad6089814d3247f42b71df0e021ffd826
A-Maruyama-dmm <com.mac.dm@gmail.com>
2021-11-18 01:54:59 +0000

    add

--- config/routes.rb
+++ config/routes.rb
@@ -1,20 +1,40 @@
 Rails.application.routes.draw do
-  devise_for :users
+
+
+ # 顧客用
+ # URL /customers/sign_in ...
+ devise_for :customers, controllers: {
+  registrations: "customer/registrations",
+  sessions: 'customer/sessions'
+ }
+ scope module: :customer do
+    get 'customer/new', to: 'customer/registrations#new'
+    get 'customer/create', to: 'customer/registrations#create'
+    get 'customer/new', to: 'customer/sessions#new'
+ end
+ # 管理者用
+ # URL /admin/sign_in ...
+ devise_for :admin, controllers: {
+  sessions: "admin/sessions"
+ }
+ namespace :admin do
+    get 'admin/new', to: 'admin/sessions#new'
+  end
 
   # 会員側のルーティング設定
-  root to: 'public/homes#top'
-  get '/about' => 'public/homes#about'
+  root to: 'homes#top'
+  get '/about' => 'homes#about'
   resources :products, only: [:index, :show]
-  resources :customers, only: [:edit, :show, :update]
-  get 'customers/unsubscribe' => 'public/customers#unsubscribe'  
-  patch 'customers/withdraw' => 'public/customers#withdraw'
+  resources :customer, only: [:edit, :show, :update]
+  get 'customer/unsubscribe' => 'customer#unsubscribe'
+  patch 'customer/withdraw' => 'customer#withdraw'
   resources :carts_items, only: [:index, :create, :destroy, :update]
-  delete 'carts_items/all_destroy' => 'public/carts_items#all_destroy'
+  delete 'carts_items/all_destroy' => 'carts_items#all_destroy'
   resources :orders, only: [:index, :create, :show, :new]
   post 'orders/comfirm' => 'orders#comfirm'
   get 'orders/complete' => 'orders#complete'
   resources :addresses, only: [:index, :create, :edit, :update, :destroy]
-  
+
   # 管理者側のルーティング設定
   namespace :admin do
     get '/admin' => 'admin/homes#top'
