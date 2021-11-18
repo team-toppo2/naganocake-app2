3d4dc15e3f4e3e2e70b5d4cd7774fda04a3520f5
ryooookada <btbl70120@gmail.com>
2021-11-18 02:17:19 +0000

    [re]homes-controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  
+
 
  # 顧客用
  # URL /customers/sign_in ...
@@ -7,34 +7,37 @@ Rails.application.routes.draw do
   registrations: "customer/registrations",
   sessions: 'customer/sessions'
  }
-
+ scope module: :customer do
+    get 'customer/new', to: 'customer/registrations#new'
+    get 'customer/create', to: 'customer/registrations#create'
+    get 'customer/new', to: 'customer/sessions#new'
+ end
  # 管理者用
  # URL /admin/sign_in ...
  devise_for :admin, controllers: {
   sessions: "admin/sessions"
  }
-  devise_for :users
+ namespace :admin do
+    get 'admin/new', to: 'admin/sessions#new'
+  end
 
   # 会員側のルーティング設定
-  scope module: :customer do
-    resources :customers
-    root to: 'homes#top'
-    get '/about' => 'homes#about'
-    resources :products, only: [:index, :show]
-    resources :customers, only: [:edit, :show, :update]
-    get 'customers/unsubscribe' => 'customers#unsubscribe'
-    patch 'customers/withdraw' => 'customers#withdraw'
-    resources :carts_items, only: [:index, :create, :destroy, :update]
-    delete 'carts_items/all_destroy' => 'carts_items#all_destroy'
-    resources :orders, only: [:index, :create, :show, :new]
-    post 'orders/comfirm' => 'orders#comfirm'
-    get 'orders/complete' => 'orders#complete'
-    resources :addresses, only: [:index, :create, :edit, :update, :destroy]
-  end
-  
+  root to: 'homes#top'
+  get '/about' => 'homes#about'
+  resources :products, only: [:index, :show]
+  resources :customers, only: [:edit, :show, :update]
+  get 'customers/unsubscribe' => 'customers#unsubscribe'
+  patch 'customers/withdraw' => 'customers#withdraw'
+  resources :carts_items, only: [:index, :create, :destroy, :update]
+  delete 'carts_items/all_destroy' => 'carts_items#all_destroy'
+  resources :orders, only: [:index, :create, :show, :new]
+  post 'orders/comfirm' => 'orders#comfirm'
+  get 'orders/complete' => 'orders#complete'
+  resources :addresses, only: [:index, :create, :edit, :update, :destroy]
+
   # 管理者側のルーティング設定
   namespace :admin do
-    get '/' => 'homes#top' #adminを削除
+    get '/' => 'homes#top'
     resources :products, only: [:index, :show, :create, :edit, :update, :new]
     resources :genres, only: [:index, :create, :edit, :update]
     resources :customers, only: [:index, :show, :edit, :update]
