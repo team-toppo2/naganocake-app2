7074e40966a732ff62369c159bf4e2ae7da88222
A-Maruyama-dmm <com.mac.dm@gmail.com>
2021-11-20 05:04:04 +0000

    costomeradd

--- app/controllers/customer/customers_controller.rb
+++ app/controllers/customer/customers_controller.rb
@@ -1,24 +1,38 @@
 class Customer::CustomersController < ApplicationController
+  def show
+    @customers = Customer.all
+    @customer = Customer.find(params[:id])
+    @new_customer = Customer.new
+  end
+
   def edit
     @customer = Customer.find(params[:id])
+    unless current_customer.id == @customer.id
+      redirect_to customer_path(current_customer)
+    end
   end
 
-  def show
-    @customer = Customer.find(params[:id])
-  end
 
   def update
-     @customer = Customer.find(paramas[:id])
-    if @customer.update(product_params)
-      redirect_to customer_path(@cutomer)
+     @customer = Customer.find(params[:id])
+    if @customer.update(customer_params)
+      redirect_to customer_path(@customer)
     else
+      @customers = Customer.all
       render edit
     end
   end
 
   def unsubscribe
+    @customers = Customer.all
+    @customer = Customer.find(params[:id])
   end
 
   def withdraw
   end
 end
+
+private
+  def customer_params
+    params.require(:customer).permit(:"氏名", :"フリガナ", :"郵便番号", :"住所", :"電話番号", :"メールアドレス")
+  end
--- app/models/customer.rb
+++ app/models/customer.rb
@@ -3,9 +3,14 @@ class Customer < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  
-  has_many :cart_items, dependent: :destroy 
-  has_many :orders, dependent: :destroy 
-  has_many :postal_addresses, dependent: :destroy 
-         
+
+  has_many :cart_items, dependent: :destroy
+  has_many :orders, dependent: :destroy
+  has_many :postal_addresses, dependent: :destroy
+
+  #def full_name
+    #self.last_name + " " + self.first_name
+  #end
+
+
 end
