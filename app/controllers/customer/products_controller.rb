a03ee84515b65a35c99bf4a47c875a0ba0fedec2
90311696+akaishisho@users.noreply.github.com <ec2-user@ip-172-31-18-152.ec2.internal>
2021-11-20 04:59:38 +0000

    [Add]products

--- app/controllers/customer/products_controller.rb
+++ app/controllers/customer/products_controller.rb
@@ -1,2 +1,13 @@
-class Public::ProductsController < ApplicationController
+class Customer::ProductsController < ApplicationController
+
+  def index
+    # 全量出すならProduct.all.page(params[:page]).per(8)
+    @products = Product.where(is_active: true).page(params[:page]).per(8)
+  end
+
+  def show
+    @product = Product.find(params[:id])
+    @cart_item = CartItem.new
+  end
+
 end
