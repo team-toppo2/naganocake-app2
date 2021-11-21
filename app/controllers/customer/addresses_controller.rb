3e06349507a83d47332dcdeac81a9872d860bf4f
90311696+akaishisho@users.noreply.github.com <ec2-user@ip-172-31-18-152.ec2.internal>
2021-11-21 04:49:11 +0000

    [Add]address

--- app/controllers/customer/addresses_controller.rb
+++ app/controllers/customer/addresses_controller.rb
@@ -1,2 +1,30 @@
 class Customer::AddressesController < ApplicationController
+  def index
+    @postal_addresses = current_customer.postal_addresses
+    @postal_address = PostalAddress.new
+  end
+
+  def create
+    @postal_address = PostalAddress.new(postal_address_params)
+    @postal_address.customer_id = current_customer.id
+    @postal_addresses =current_customer.postal_address
+    if @postal_address.save
+      redirect_to addresses_path
+    else
+      @postal_addresses =current_customer.postal_address
+      render index
+    end
+  end
+
+  def edit
+    @postal_address = PostalAddress.find(params[:id])
+  end
+
+  def update
+  end
+
+  def destroy
+    @postal_address = PostalAddress.find(params[:id])
+  end
+
 end
