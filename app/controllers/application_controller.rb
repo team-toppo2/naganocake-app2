28e6f7dd42a5e7e4af06326293c09f9d3da0d0d8
A-Maruyama-dmm <com.mac.dm@gmail.com>
2021-11-21 04:48:23 +0000

    add

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,21 @@
 class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
 
+
+  def create
+    @customer = Customer.new(customer_params)
+    @customer.customer_id = current_customer.id
+    if @customer.save
+      redirect_to customer_path(@customer)
+    else
+      @customers = Customer.all
+      render 'index'
+    end
+  end
+
   protected
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:last_name, :first_name, :last_name_jp, :first_name_jp, :postal_code, :address, :telephone_number, :password])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:last_name, :first_name, :last_name_jp, :first_name_jp, :postal_code, :address, :telephone_number])
   end
 end
