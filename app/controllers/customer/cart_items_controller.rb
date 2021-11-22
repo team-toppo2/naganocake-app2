5baa7bbacd984dbef46ff190bdc61f2228ee5a75
rikuosh <0429riku@gmail.com>
2021-11-22 04:00:54 +0000

    add

--- app/controllers/customer/cart_items_controller.rb
+++ app/controllers/customer/cart_items_controller.rb
@@ -2,7 +2,6 @@ class Customer::CartItemsController < ApplicationController
   
   def index
     @cart_items = current_customer.cart_items.all
-    @total = @cart_items.inject(0) { |sum, product| sum + product.subtotal }
   end
   
   def create
@@ -12,10 +11,10 @@ class Customer::CartItemsController < ApplicationController
     #ログイン中カスタマーのカートアイテムを全て取り出す
     @cart_items.each do |cart_item|
     #カートの中に入れた商品情報が既にカートにある場合は
-     if cart_item.product_id == @cart_item.item_id
+     if cart_item.product_id == @cart_item.product_id
     #既にカートにあるamountと現在追加したamountを足して更新する
-       new_amount = cart_item.amount + @cart_item.amount
-       cart_item.update_attribute(:amount, new_amount)
+       new_product_amount = cart_item.product_amount + @cart_item.product_amount
+       cart_item.update_attribute(:product_amount, new_product_amount)
        @cart_item.delete
      end
     end
@@ -36,9 +35,13 @@ class Customer::CartItemsController < ApplicationController
   end
 
  def cart_items_all_destory
-   cart_items = CartItem.all
+   cart_items = current_customer.cart_items.all
    cart_items.destroy_all
    render 'index'
+ end
+ 
+ def cart_item_params
+    params.require(:cart_item).permit(:customer_id, :product_id, :product_amount)
  end
  
 end
--- app/models/cart_item.rb
+++ app/models/cart_item.rb
@@ -3,7 +3,7 @@ class CartItem < ApplicationRecord
   belongs_to :product
   
   def subtotal
-    product.add_tax_price * amount
+    product.add_tax_price * product_amount
   end
   
 end
