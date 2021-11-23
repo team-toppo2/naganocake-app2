1798c572451e1eac224794d0d30f2b6d1c2b01ae
ryooookada <btbl70120@gmail.com>
2021-11-23 08:51:21 +0000

    add

--- app/controllers/admin/sessions_controller.rb
+++ app/controllers/admin/sessions_controller.rb
@@ -25,7 +25,7 @@ class Admin::SessionsController < Devise::SessionsController
   end
 
   def after_sign_out_path_for(resource)
-    admin_path
+    new_admin_session_path
   end
 
   def configure_permitted_parameters
