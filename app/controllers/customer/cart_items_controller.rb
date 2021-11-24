192646e4bed0a5f301df31736a177723893d1626
rikuosh <0429riku@gmail.com>
2021-11-24 09:06:06 +0000

    add

--- app/controllers/customer/cart_items_controller.rb
+++ app/controllers/customer/cart_items_controller.rb
@@ -38,7 +38,7 @@ class Customer::CartItemsController < ApplicationController
  def all_destroy
    cart_items = current_customer.cart_items.all
    cart_items.destroy_all
-   redirect_to cart_items
+   redirect_to cart_items_path
  end
 
  def cart_item_params
